c91efcfb027beec82caa7c07ee032c98f1b6dd7f
Lina Shadrach <lina.shadrach@gmail.com>
2017-02-16 23:59:14 -0800

    add first test

---
+++ SonOfCodSeafood.Tests/ModelTests/RecipientTest.cs
@@ -0,0 +1,19 @@
+﻿using SonOfCodSeafood.Models;
+using Xunit;
+
+namespace SonOfCodSeafood.Tests
+{
+
+    public class RecipientTest
+    {
+        [Fact]
+        public void Recipient_IsCreated_Test()
+        {
+            Recipient recipient = new Recipient(0, "Lila", "lila@mai.com", 99654, 0);
+            Recipient otherRecipient = new Recipient(0, "Lila", "lila@mai.com", 99654, 0);
+            Assert.Equal(recipient, otherRecipient);
+        }
+
+    }
+}
+
---
+++ SonOfCodSeafood.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SonOfCodSeafood.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f3f73f2-8876-49f8-9bb3-d9e4eb22f338")]
